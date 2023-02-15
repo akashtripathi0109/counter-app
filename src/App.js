8d19eeee97d235e79220575dc5797419c783b67f
akashtripathi0109 <57222581+akashtripathi0109@users.noreply.github.com>
2023-02-15 22:18:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Segment, Grid, Header, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => count > 0 && setCount(count - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Segment textAlign='center' style={{ padding: '130px', width:'100wh' , height:'100vh', backgroundImage: 'url(https://miro.medium.com/v2/resize:fit:828/0*nvvPWlrPpX2e_Vkn)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', opacity: '0.8' }}>
+      <Header as='h1'style={{padding : '30px', color: 'yellow'}}>COUNTER APP</Header>
+      <Segment textAlign='center' style={{ padding: '40px', boxShadow: '#ccc',height: '55%',width: '40%', left : '373px' }}>
+        <Header as='h2'>Count</Header>
+        <h1 style={{ color: count < 5 ? "green" : count < 10 ? "blue" : "red" }}>{count}</h1>
+        <Grid columns='equal'>
+          <Grid.Row>
+            <Grid.Column>
+              <Button color='green' content='Increment' onClick={increment} disabled={count === 10}/>
+            </Grid.Column>
+            <Grid.Column>
+              <Button color='red' content='Decrement'  onClick={decrement} disabled={count === 0}/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as='h2' style={{padding : '70px', right :'5px', color: 'yellow'}}>Done by : Akash Tripathi - 12101602</Header>
+    </Segment>
   );
-}
+};
 
 export default App;
